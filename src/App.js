0c99e3c27027b04771d94cb6e709c941901243ba
Anshika <anshikasethiya28@gmail.com>
2021-06-04 16:17:38 +0530

    Created private and public route & setting up firebase interaction for signin page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss'
+import { Switch } from 'react-router';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return (
-    <div>
-      App page
-    </div>
-  );
+  return(
+     <Switch>
+        <PublicRoute path="/signin">
+           <SignIn />
+        </PublicRoute>
+      
+
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+      </Switch>
+  )
 }
 
 export default App;
---
+++ src/misc/Firebase.js
@@ -0,0 +1,17 @@
+   import firebase from 'firebase/app'
+   import 'firebase/auth'
+   import 'firebase/database'
+   
+   const config = {
+    apiKey: "AIzaSyASzyUb0Hf5oJyMsw5JNvR9RvncBQ36mSE",
+    authDomain: "chat-web-app-96ce5.firebaseapp.com",
+    databaseURL: "https://chat-web-app-96ce5-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-96ce5",
+    storageBucket: "chat-web-app-96ce5.appspot.com",
+    messagingSenderId: "213342160296",
+    appId: "1:213342160296:web:e729ef174bcd9860a9899d"
+  };
+
+  const app = firebase.initializeApp(config)
+  export const auth = app.auth();
+  export const database = app.database()
